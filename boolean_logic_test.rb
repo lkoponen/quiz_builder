4947fec56c1449ef1cca358d19ef9ba7cd8ed63c
Lili Koponen <ljkoponen@gmail.com>
2013-05-03 20:36:07 -0700

    Moved questions into separate require file

--- boolean_logic_test.rb
+++ boolean_logic_test.rb
@@ -1,29 +1,7 @@
-questions = [{:question => "not False", :answer => "True"}, 
-			 {:question => "not True", :answer => "False"},
-			 {:question => "True or False", :answer => "True"},
-			 {:question => "True or True", :answer => "True"},
-			 {:question => "False or True", :answer => "True"},
-			 {:question => "False or False", :answer => "False"},
-			 {:question => "True and False", :answer => "False"},
-			 {:question => "True and True", :answer => "True"},
-			 {:question => "False and True", :answer => "False"},
-			 {:question => "False and False", :answer => "False"},
-			 {:question => "not (True or False)", :answer => "False"},
-			 {:question => "not (True or True)", :answer => "False"},
-			 {:question => "not (False or True)", :answer => "False"},
-			 {:question => "not (False or False)", :answer => "True"},
-			 {:question => "not (True and False)", :answer => "True"},
-			 {:question => "not (True and True)", :answer => "False"},
-			 {:question => "not (False and True)", :answer => "True"},
-			 {:question => "not (False and False)", :answer => "True"},
-			 {:question => "1 != 0", :answer => "True"},
-			 {:question => "1 != 1", :answer => "False"},
-			 {:question => "0 != 1", :answer => "True"},
-			 {:question => "0 != 0", :answer => "False"},
-			 {:question => "1 == 0", :answer => "False"},
-			 {:question => "1 == 1", :answer => "True"},
-			 {:question => "0 == 1", :answer => "False"},
-			 {:question => "0 == 0", :answer => "True"}]
+require './questions'
+
+questions = Questions.new
+questions = questions.list
 
 prompt = "> "
 
@@ -31,5 +9,5 @@ questions.each do |problem|
 	puts problem[:question]
 	print prompt
 	answer = gets.chomp()
-	puts problem[:answer]
+	puts "The correct answer is: #{problem[:answer]}"
 end
---
+++ questions.rb
@@ -0,0 +1,31 @@
+class Questions
+
+def list
+	questions = [{:question => "not False", :answer => "True"}, 
+			 {:question => "not True", :answer => "False"},
+			 {:question => "True or False", :answer => "True"},
+			 {:question => "True or True", :answer => "True"},
+			 {:question => "False or True", :answer => "True"},
+			 {:question => "False or False", :answer => "False"},
+			 {:question => "True and False", :answer => "False"},
+			 {:question => "True and True", :answer => "True"},
+			 {:question => "False and True", :answer => "False"},
+			 {:question => "False and False", :answer => "False"},
+			 {:question => "not (True or False)", :answer => "False"},
+			 {:question => "not (True or True)", :answer => "False"},
+			 {:question => "not (False or True)", :answer => "False"},
+			 {:question => "not (False or False)", :answer => "True"},
+			 {:question => "not (True and False)", :answer => "True"},
+			 {:question => "not (True and True)", :answer => "False"},
+			 {:question => "not (False and True)", :answer => "True"},
+			 {:question => "not (False and False)", :answer => "True"},
+			 {:question => "1 != 0", :answer => "True"},
+			 {:question => "1 != 1", :answer => "False"},
+			 {:question => "0 != 1", :answer => "True"},
+			 {:question => "0 != 0", :answer => "False"},
+			 {:question => "1 == 0", :answer => "False"},
+			 {:question => "1 == 1", :answer => "True"},
+			 {:question => "0 == 1", :answer => "False"},
+			 {:question => "0 == 0", :answer => "True"}]
+	end
+end
